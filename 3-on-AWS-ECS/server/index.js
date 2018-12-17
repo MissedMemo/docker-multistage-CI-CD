b781f91ff52a612f0c0102bc202bf5ddbca53ad4
AndyL <missedmemo@gmail.com>
2018-12-17 09:49:40 -0800

    copy existing project folder to create new project base

---
+++ 3-on-AWS-ECS/server/index.js
@@ -0,0 +1,16 @@
+"use strict"
+
+const express = require('express')
+const app = express()
+
+app.use( express.static('dist/') )
+
+const port = process.env.PORT || 3000
+
+app.listen( port, () => {
+  console.log(`
+  //////////////////////////////////
+     listening on port ${port}
+  //////////////////////////////////
+  `)
+})
