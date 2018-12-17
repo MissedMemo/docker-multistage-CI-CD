5fc12c182ea2dd447d5d78ebc264029bb7d2979f
AndyL <missedmemo@gmail.com>
2018-12-16 21:54:36 -0800

    create copy of subproject as base for new project

---
+++ 2-hello-React/server.js
@@ -0,0 +1,18 @@
+"use strict"
+
+const express = require('express')
+const app = express()
+
+const port = process.env.PORT || 3000
+
+app.get( '/', (req,res) => {
+  res.send('Hello, express!')
+})
+
+app.listen( port, () => {
+  console.log(`
+  //////////////////////////////////
+     listening on port ${port}
+  //////////////////////////////////
+  `)
+})
